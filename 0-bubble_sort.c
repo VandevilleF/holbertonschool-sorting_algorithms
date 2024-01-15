27dc69342316a2abc81026ca472826e107efa4ab
VandevilleF <flo.vandeville@gmail.com>
2024-01-15 14:43:55 +0100

    Add "- i" in second loop

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,10 +12,10 @@ void bubble_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
-		
+
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
